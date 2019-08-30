c4607a8046f9f0f25df53646d496e24863173b29
Ridhwana <ridhwana.khan16@gmail.com>
2019-08-30 12:21:56 +0200

    feat: add the dob

--- src/main/java/smile/identity/core/IDParameters.java
+++ src/main/java/smile/identity/core/IDParameters.java
@@ -4,7 +4,7 @@ import org.json.simple.JSONObject;
 public class IDParameters implements Parameters {
   JSONObject idInfo;
 
-  public IDParameters(String first_name, String middle_name, String last_name, String country, String id_type, String id_number, String entered) {
+  public IDParameters(String first_name, String middle_name, String last_name, String country, String id_type, String id_number, String dob, String entered) {
     JSONObject obj = new JSONObject();
 
     try {
@@ -14,6 +14,7 @@ public class IDParameters implements Parameters {
       obj.put("country", country);
       obj.put("id_type", id_type);
       obj.put("id_number", id_number);
+      obj.put("dob", dob);
       obj.put("entered", entered);
 
       if(entered == "true") {
@@ -24,6 +25,11 @@ public class IDParameters implements Parameters {
         if (first_name.trim().isEmpty() || last_name.trim().isEmpty() || country.trim().isEmpty() || id_type.trim().isEmpty() || id_number.trim().isEmpty()) {
           throw new IllegalArgumentException("ID Arguments may not be empty");
         }
+
+        if ((country == "NG") &&  (id_type == "PASSPORT" ||  id_type == "VOTER_ID" || id_type == "DRIVERS_LICENSE" || id_type == "DRIVERS_LICENSE" || id_type == "TIN" || id_type == "CAC") && (dob.trim().isEmpty())) {
+          throw new IllegalArgumentException("The ID type " + id_type + "for " + country + " requires a valid dob paramater." );
+        }
+
       }
 
     } catch(Exception e) {
