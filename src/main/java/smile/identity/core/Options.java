ee83d7c2b42e5923943fce87021c74600db1932c
Ridhwana <ridhwana.khan16@gmail.com>
2019-09-04 19:29:52 +0200

    feat: method-overloading for options (allow options to take two parameters instead of writing a whole new class)

--- src/main/java/smile/identity/core/Options.java
+++ src/main/java/smile/identity/core/Options.java
@@ -4,14 +4,19 @@ import org.json.simple.JSONObject;
 public class Options implements Parameters {
   JSONObject options;
 
+  public Options(Boolean return_history, Boolean return_images) {
+    this(null, null, return_history, return_images);
+  }
+
   public Options(String optional_callback, Boolean return_job_status, Boolean return_history, Boolean return_images) {
     JSONObject obj = new JSONObject();
 
     try {
-      obj.put("optional_callback", optional_callback);
-      obj.put("return_job_status", return_job_status);
-      obj.put("return_history", return_history);
-      obj.put("return_images", return_images);
+      if (!checkNullAndEmpty(optional_callback)) obj.put("optional_callback", optional_callback);
+      if (!checkNull(return_job_status)) obj.put("return_job_status", return_job_status);
+      if (!checkNull(return_history)) obj.put("return_history", return_history);
+      if (!checkNull(return_images)) obj.put("return_images", return_images);
+
     } catch (Exception e) {
       throw e;
     }
@@ -23,4 +28,13 @@ public class Options implements Parameters {
   public String get() {
     return options.toString();
   }
+
+  private Boolean checkNullAndEmpty(String value) {
+    return value == null || value.trim().isEmpty();
+  }
+
+  private Boolean checkNull(Boolean value) {
+    return value == null;
+  }
+
 }
