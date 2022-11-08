47b32b690a69f41ed9455c9710da3bdd1109bf73
Destiny Gaines <destiny@smileidentity.com>
2022-11-08 16:52:00 -0500

    Refactoring Signature class

--- src/test/java/smile/identity/core/SignatureTest.java
+++ src/test/java/smile/identity/core/SignatureTest.java
@@ -1,81 +1,29 @@
 package smile.identity.core;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.Date;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.junit.Before;
 import org.junit.Test;
+import smile.identity.core.keys.SignatureKey;
 
 public class SignatureTest {
-	
-    private String PARTNER_ID = "<PARTNER_ID>";
-    private String API_KEY = "<API_KEY>";
-    private Signature mSignature;
-	
-    @Before
-    public void setup() {
-    	mSignature = new Signature(PARTNER_ID, API_KEY);
-    }
-
-    @Test
-    public void testSignature() {
-    	JSONObject sigJsonObj = null;
-    	Long dateTime = new Date().getTime();
-    	
-    	try {
-        	sigJsonObj = (JSONObject) new JSONParser().parse(mSignature.generate_signature(dateTime));
-    	} catch (Exception e) {
-    		assertTrue(false);
-		}
-    	
-    	assertNotNull(sigJsonObj);
-    	assertTrue(sigJsonObj.containsKey(Signature.TIME_STAMP_KEY));
-    	
-    	Long dt = (Long) sigJsonObj.get(Signature.TIME_STAMP_KEY);
-    	assertEquals(dateTime, dt);
-    	
-    	assertTrue(sigJsonObj.containsKey(Signature.SIGNATURE_KEY));
-    	String signature = (String) sigJsonObj.get(Signature.SIGNATURE_KEY);
-    	assertTrue(!signature.isBlank());
-    	
-    	assertTrue(mSignature.confirm_signature(dateTime, signature));
-    }
 
 	@Test
-	public void testUseSignatureDefaultTrue(){
-		JSONObject options = new JSONObject();
-		Boolean useSignature = Signature.useSignature(options);
-		assert(useSignature).equals(true);
+	public void itGeneratesASignatureWhenNoTimestampProvided(){
+		Signature signature = new Signature("partner", "apiKey");
+		SignatureKey sk = signature.generateSignature();
+		assert!(sk.getSignature()).isEmpty();
 	}
 
 	@Test
-	public void testUseSignatureUseOptionFalse(){
-		JSONObject options = new JSONObject();
-		options.put(Signature.SIGNATURE_KEY, false);
-		Boolean useSignature = Signature.useSignature(options);
-		assert(useSignature).equals(false);
+	public void itGeneratesASignatureWhenTimestampProvided(){
+		Signature signature = new Signature("partner", "apiKey");
+		Long timestamp = System.currentTimeMillis();
+		SignatureKey sk = signature.generateSignature();
+		assert(sk.getTimestamp().equals(timestamp));
 	}
 
 	@Test
-	public void testUseSignatureUseOptionTrue(){
-		JSONObject options = new JSONObject();
-		options.put(Signature.SIGNATURE_KEY, true);
-		Boolean useSignature = Signature.useSignature(options);
-		assert(useSignature).equals(true);
+	public void itConfirmsASignature(){
+		Signature signature = new Signature("partner", "apiKey");
+		SignatureKey original = signature.generateSignature();
+		assert(signature.validateSignature(original.getTimestamp(), original.getSignature()));
 	}
-
-	@Test
-	public void testSignatureAndSecKeyPresent(){
-		JSONObject options = new JSONObject();
-		options.put(Signature.SIGNATURE_KEY, true);
-		options.put(Signature.SEC_KEY, true);
-		Boolean useSignature = Signature.useSignature(options);
-		assert(useSignature).equals(true);
-	}
-
 }
