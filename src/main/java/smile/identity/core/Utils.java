58162c58f51ce029ccfc93df5e5cc717073aab26
Destiny Gaines <destiny@smileidentity.com>
2022-11-18 18:41:43 -0500

    Updating readme, test & utils

--- src/main/java/smile/identity/core/Utils.java
+++ src/main/java/smile/identity/core/Utils.java
@@ -12,4 +12,14 @@ public class Utils {
     public static String getApiVersion(){
         return properties.getProperty("apiVersion");
     }
+
+    public static String getSidServer(String sidServer) {
+        if(sidServer.equals("0")) {
+            return "https://3eydmgh10d.execute-api.us-west-2.amazonaws.com/test";
+        } else if (sidServer.equals("1")) {
+            return "https://la7am6gdm8.execute-api.us-west-2.amazonaws.com/prod";
+        } else {
+            return sidServer;
+        }
+    }
 }
