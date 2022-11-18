c921a927041b9f32be54a764ef9980bcc44adbeb
Destiny Gaines <destiny@smileidentity.com>
2022-11-18 17:25:35 -0500

    adding method to get sidserver

--- src/main/java/smile/identity/core/Utils.java
+++ src/main/java/smile/identity/core/Utils.java
@@ -5,11 +5,21 @@ import java.util.Properties;
 public class Utils {
     private final static Properties properties = new Properties();
 
-    public static String getVersion(){
+    public static String getVersion() {
         return properties.getProperty("version");
     }
 
-    public static String getApiVersion(){
+    public static String getApiVersion() {
         return properties.getProperty("apiVersion");
+    }
+
+    public static String getSidServer(String sidServer) {
+        if(sidServer.equals("0")) {
+            return "https://3eydmgh10d.execute-api.us-west-2.amazonaws.com/test";
+        } else if (sidServer.equals("1")) {
+            return "https://la7am6gdm8.execute-api.us-west-2.amazonaws.com/prod";
+        } else {
+            return sidServer;
+        }
     }
 }
