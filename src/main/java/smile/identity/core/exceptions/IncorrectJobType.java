40e9056b6e14fc8f69600147f742c3af846ac91e
Destiny Gaines <destiny@smileidentity.com>
2022-11-18 17:28:34 -0500

    Updating idapi class & adding test

--- src/main/java/smile/identity/core/exceptions/IncorrectJobType.java
+++ src/main/java/smile/identity/core/exceptions/IncorrectJobType.java
@@ -3,7 +3,7 @@ package smile.identity.core.exceptions;
 public class IncorrectJobType extends Exception {
 
     public IncorrectJobType(int jobType, String service) {
-        super(String.format("Please ensure that you are setting your job_type" +
+        super(String.format("Please ensure that you are setting your jobType" +
                 " to %d for %s", jobType, service));
     }
 }
