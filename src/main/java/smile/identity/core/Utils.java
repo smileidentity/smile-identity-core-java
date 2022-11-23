12b350b98543a9e4145ab992989f3f1266a9a02c
Destiny Gaines <destiny@smileidentity.com>
2022-11-23 17:13:29 -0500

    Using subdomain entries

--- src/main/java/smile/identity/core/Utils.java
+++ src/main/java/smile/identity/core/Utils.java
@@ -14,10 +14,10 @@ public class Utils {
     }
 
     public static String getSidServer(String sidServer) {
-        if(sidServer.equals("0")) {
-            return "https://3eydmgh10d.execute-api.us-west-2.amazonaws.com/test";
+        if (sidServer.equals("0")) {
+            return "https://testapi.smileidentity.com";
         } else if (sidServer.equals("1")) {
-            return "https://la7am6gdm8.execute-api.us-west-2.amazonaws.com/prod";
+            return "https://api.smileidentity.com";
         } else {
             return sidServer;
         }
