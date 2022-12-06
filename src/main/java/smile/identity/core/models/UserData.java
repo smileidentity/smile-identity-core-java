4bc285c59510844ed8d84f2d5726772a2f7a85b9
Destiny Gaines <destiny@smileidentity.com>
2022-12-06 18:54:26 -0500

    So long, Bill

--- src/main/java/smile/identity/core/models/UserData.java
+++ src/main/java/smile/identity/core/models/UserData.java
@@ -18,7 +18,7 @@ public class UserData {
     String countryName;
 
     public UserData(){
-        this(false, "", "", "", "Bill", "", "", "", "+", "");
+        this(false, "", "", "", "", "", "", "", "+", "");
     }
 
 }
