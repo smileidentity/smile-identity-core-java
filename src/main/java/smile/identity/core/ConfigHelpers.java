cb57f99bca673bee75d1bedfba523dccbc449fc4
Destiny Gaines <destiny@smileidentity.com>
2023-01-31 14:15:43 -0500

    moving versions to api

--- src/main/java/smile/identity/core/ConfigHelpers.java
+++ src/main/java/smile/identity/core/ConfigHelpers.java
@@ -4,8 +4,8 @@ package smile.identity.core;
 public class ConfigHelpers {
     private final static String TEST_ENV = "0";
     private final static String PROD_ENV = "1";
-    private final static String TEST_SERVER = "https://testapi.smileidentity.com/v1/";
-    private final static String PROD_SERVER = "https://api.smileidentity.com/v1/";
+    private final static String TEST_SERVER = "https://testapi.smileidentity.com/";
+    private final static String PROD_SERVER = "https://api.smileidentity.com/";
 
     public static String getVersion() {
         return "2.0.2";
