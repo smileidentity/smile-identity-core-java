2e3f245a02ec61924c0db8920e5c043ae2606435
Juma Allan <allanjuma@gmail.com>
2025-06-26 18:50:43 +0300

    added nullable Antifraud

--- src/main/java/smile/identity/core/models/JobResponse.java
+++ src/main/java/smile/identity/core/models/JobResponse.java
@@ -1,14 +1,15 @@
 package smile.identity.core.models;
 
 import com.squareup.moshi.Json;
-
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import lombok.experimental.NonFinal;
+
+import javax.annotation.Nullable;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Value
 @NonFinal
@@ -48,12 +49,57 @@ public class JobResponse {
     @Json(name = "Source")
     String source;
 
+    @Nullable
+    @Json(name = "Antifraud")
+    Antifraud antifraud;
+
     @Json(name = "FullData")
     Map<String, Object> fullData;
 
     public JobResponse(String smileJobId, PartnerParams partnerParams) {
         this("", smileJobId, partnerParams, "", "", "", "", null, "", null,
-                "", "", new HashMap<>());
+                "", "", null, new HashMap<>());
     }
 
+    @Value
+    @NonFinal
+    @AllArgsConstructor
+    public static class Antifraud {
+        @Json(name = "summary")
+        public Summary summary;
+        @Json(name = "smile_secure")
+        public SmileSecure smileSecure;
+    }
+
+    @Value
+    @NonFinal
+    @AllArgsConstructor
+    public static class Summary {
+        @Json(name = "fraud_sources")
+        public List<String> fraudSources;
+        @Json(name = "fraud_detected")
+        public Boolean fraudDetected;
+    }
+
+    @Value
+    @NonFinal
+    @AllArgsConstructor
+    public static class SmileSecure {
+        @Json(name = "ResultCode")
+        public String resultCode;
+        @Json(name = "ResultText")
+        public String resultText;
+        @Json(name = "SuspectUsers")
+        public List<SuspectUser> suspectUsers;
+    }
+
+    @Value
+    @NonFinal
+    @AllArgsConstructor
+    public static class SuspectUser {
+        @Json(name = "reasons")
+        public List<String> reasons;
+        @Json(name = "user_id")
+        public String userId;
+    }
 }
